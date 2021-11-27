ff44b9588af3f4cf355db148c0c269f0390a3392
sarojvrc <saroj.vrc@gmail.com>
2021-11-27 11:22:38 +0530

    footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <footer>Tic Tac Toe made by Saroj</footer>
+        </div>
+    )
+}
+
+export default Footer
