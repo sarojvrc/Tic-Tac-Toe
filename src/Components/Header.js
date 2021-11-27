bacde7ed3eb2f477091a10e383db487278073811
sarojvrc <saroj.vrc@gmail.com>
2021-11-27 11:22:46 +0530

    header added

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <header>Tic Tac Toe</header>
+        </div>
+    )
+}
+
+export default Header
